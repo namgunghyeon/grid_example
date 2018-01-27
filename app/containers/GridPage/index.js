de4ae6aa50ffc961969e4f9f7d3c0d7ed07638fe
남궁현 <nkh@encoredtech.com>
2018-01-27 18:28:39 +0900

    Add grid example

--- app/containers/GridPage/index.js
+++ app/containers/GridPage/index.js
@@ -4,9 +4,34 @@
  * List all the features
  */
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
 
 import H1 from 'components/H1';
+
+const ResponsiveWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 20px;
+  color: #444;
+`;
+
+const FixedWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 100px 100px 100px;
+  grid-gap: 20px;
+  color: #444;
+`;
+
+
+const Box = styled.div`
+  background-color: #444;
+  color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  font-size: 150%;
+  text-align: center;
+`;
 
 export default class GridPage extends Component { // eslint-disable-line react/prefer-stateless-function
 
@@ -20,12 +45,31 @@ export default class GridPage extends Component { // eslint-disable-line react/p
     return (
       <div>
         <Helmet>
-          <title>Feature Page</title>
-          <meta name="description" content="Feature page of React.js Boilerplate application" />
+          <title>Grid Page</title>
+          <meta name="description" content="Grid page of React.js Boilerplate application" />
         </Helmet>
         <H1>
-          {'123'}
+          {'Responsive grid '}
         </H1>
+        <ResponsiveWrapper>
+          <Box>A</Box>
+          <Box>B</Box>
+          <Box>C</Box>
+          <Box>D</Box>
+          <Box>E</Box>
+          <Box>F</Box>
+        </ResponsiveWrapper>
+        <H1>
+          {'Fixed grid'}
+        </H1>
+        <FixedWrapper>
+          <Box>A</Box>
+          <Box>B</Box>
+          <Box>C</Box>
+          <Box>D</Box>
+          <Box>E</Box>
+          <Box>F</Box>
+        </FixedWrapper>
       </div>
     );
   }
